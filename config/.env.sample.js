fe0b0405d069b1ec9274d2b9812ad4c4b82c7f61
Eric Carmona <ericscarmona@yahoo.com>
2018-03-18 22:21:51 -0700

    [client] installed necessary dependencies and and setup env

---
+++ config/.env.sample.js
@@ -0,0 +1,12 @@
+const envBuild = {
+  'server': [
+    'PORT=1337'
+  ],
+  'client': [
+    'NODE_ENV=TEST',
+    'REST_SERVER_URL=http://localhost:3396',
+    //'SOCKET_SERVER_URL=http://localhost:4155',
+    'DEV_REST_SERVER_URL=http://54.183.137.202:3396',
+    //'DEV_SOCKET_SERVER_URL=http://54.183.137.202:4155',
+  ]
+}
