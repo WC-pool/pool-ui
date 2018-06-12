5ed5e99b14c3c8521a5f57235d922049695e7633
Eric Carmona <ericscarmona@yahoo.com>
2018-06-11 18:48:29 -0700

    [client] dynamically render auth components

--- client/src/components/Nav/NavBar.jsx
+++ client/src/components/Nav/NavBar.jsx
@@ -2,19 +2,59 @@ import 'babel-polyfill';
 import React from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
+import url from '../../config';
+import firebase from 'firebase';
+import Home from '../Home/Home';
 
 class NavBar extends React.Component {
   constructor() {
     super()
 
-    
+  this.onSuccess = this.onSuccess.bind(this);
   }
   componentDidMount () {
-    console.log('apikey', process.env.FIREBASE_API_KEY)
+    
   }
 
+  onSuccess() {
+    this.props.history.push('/');
+  }
+
+  async onLogoutHandler() {
+    try {
+      const logout = await firebase.auth().signOut()
+      localStorage.removeItem('activeUid');
+      window.location.reload(true);
+      this.props.history.push('/');
+    } catch (err) {
+      console.log('Error logging out');
+    }
+  }
+
+
+
   render() {
-    return (<h4>NavBar</h4>)
+    return (
+      <div>
+      {
+        !localStorage.getItem('activeUid') ?
+        <div>
+        <Link to='/login'><button>Login</button></Link>
+        <p></p>
+        <Link to='/signup'><button>Signup</button></Link>
+        </div>
+        :
+        <div>
+          <div>
+          <button onClick={this.onLogoutHandler.bind(this)}>Logout</button>
+          </div>
+          <div>
+          <Home />
+          </div>
+        </div>
+      }
+      </div>
+    )
   }
 }
 
