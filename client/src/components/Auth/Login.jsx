641817a59fd92d2bf71b8fd744707b44af1e7622
Eric Carmona <ericscarmona@yahoo.com>
2018-06-29 21:36:29 -0700

    [login] login thru firebase auth functional

--- client/src/components/Auth/Login.jsx
+++ client/src/components/Auth/Login.jsx
@@ -21,25 +21,26 @@ class Login extends React.Component {
     try {
       const authData = await firebase.auth().signInWithPopup(googleProvider)
       await localStorage.setItem('activeUid', authData.user.uid);
-
+      console.log('AUTHDATA', authData)
       let payload = {
-        email: authData.user.email,
         name: authData.user.displayName,
+        email: authData.user.email,
         uid: authData.user.uid,
         image: authData.user.photoURL
       }
       try {
         const data = await axios.post(`${url.restServer}/api/auth/signup`, payload)
         try {
-           const data = await axios.get(`${url.restServer}/api/users/getUser`, {params: {uid: localStorage.getItem('activeUid')}})
-           await localStorage.setItem('activeUid', data.data.rows[0].uid)
-           await localStorage.setItem('activeId', data.data.rows[0].id)
-           await localStorage.setItem('name', data.data.rows[0].name)
-           await localStorage.setItem('email', data.data.rows[0].email)
-           await localStorage.setItem('accountType', data.data.rows[0].type)
-           await localStorage.setItem('profilePictureURL', data.data.rows[0].image)
-           await this.props.setActiveUser(data.data.rows[0])
-           await this.props.setUserData(data.data.rows[0])
+           const userData = await axios.get(`${url.restServer}/api/users/getUser`, {params: {uid: localStorage.getItem('activeUid')}})
+           console.log('dAta', data)
+           await localStorage.setItem('activeUid', userData.data.rows[0].uid)
+           //await localStorage.setItem('activeId', data.data.rows[0].id)
+           await localStorage.setItem('name', userData.data.rows[0].name)
+           await localStorage.setItem('email', userData.data.rows[0].email)
+           //sawait localStorage.setItem('accountType', data.data.rows[0].type)
+           await localStorage.setItem('profilePictureURL', userData.data.rows[0].image)
+//await this.props.setActiveUser(data.data.rows[0])
+  //         await this.props.setUserData(data.data.rows[0])
            setTimeout(()=>{this.onSuccess()}, 2000)
         } catch (err) {
           console.log('error...', err)
@@ -59,6 +60,7 @@ class Login extends React.Component {
       <div>
         <h4>Login</h4>
       </div>
+      <div>{console.log('url', url.restServer)}</div>
       <div>
         <button onClick={this.onGoogleClickHandler.bind(this)}>Google</button>
       </div>
