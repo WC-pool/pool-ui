5dc4677686b002c3d73a1a6eefe33b97773986ac
Eric Carmona <ericscarmona@yahoo.com>
2018-07-25 16:47:23 -0700

    [client] instantiating socket on chat component

--- client/src/components/Chat/Chat.jsx
+++ client/src/components/Chat/Chat.jsx
@@ -1,13 +1,47 @@
 import 'babel-polyfill';
 import React from 'react';
+import axios from 'axios';
+import url from '../../config';
+import io from 'socket.io-client';
+
 
 class Chat extends React.Component {
   constructor() {
     super()
+    
+    this.state = {
+      socket: null,
+      messages: [],
+      author: null,
+      message: ''
+    }
+  }
+
+  async componentWillMount () {
+
+    const socket = io(`${url.socketServer}`);
+
+    this.setState({socket: socket});
+
+    try {
+      const data = await axios.get(`${url.socketServer}/api/chat/getMessages`);
+    } catch (err) {
+      console.log('Error getting messages', err);
+    }
+
+
+  }
+
+  sendMessage (e) {
+    e.preventDefault();
+    
   }
 
   render() {
-   return (<h4>Chat</h4>)
+   return (<div>
+    <h4>Chat</h4>
+    {console.log('sockit to me',this.state.socket)}
+    </div>)
   }
 }
 export default Chat;
